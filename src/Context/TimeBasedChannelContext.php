560adaaeccf9b3e12f0d34562fa32ae0d8bcae08
janpalen <jan.palen@bitbag.pl>
2024-07-02 12:34:34 +0200

    BIT-492 - lesson 2.1

--- src/Context/TimeBasedChannelContext.php
+++ src/Context/TimeBasedChannelContext.php
@@ -4,14 +4,25 @@ declare(strict_types=1);
 
 namespace App\Context;
 
+use App\DateTime\ClockInterface;
 use Sylius\Component\Channel\Context\ChannelContextInterface;
 use Sylius\Component\Channel\Model\ChannelInterface;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 
-class TimeBasedChannelContext implements ChannelContextInterface
+final class TimeBasedChannelContext implements ChannelContextInterface
 {
+    public function __construct(
+        private readonly ChannelRepositoryInterface $channelRepository,
+        private readonly ClockInterface $clock,
+    ) {
+    }
 
     public function getChannel(): ChannelInterface
     {
-        // TODO: Implement getChannel() method.
+        if ($this->clock->isNight()) {
+            return $this->channelRepository->findOneByCode('NIGHT');
+        }
+
+        return $this->channelRepository->findOneBy([]);
     }
 }
